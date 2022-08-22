c497d18000814f7db58cadacbd1b1fc1a615e1b1
ralba <ralba_ext@yaganaste.com>
2022-08-22 15:50:34 -0500

    actualizar gladle

--- QposLib/src/main/java/com/pagatodo/qposlib/QPosManager.java
+++ QposLib/src/main/java/com/pagatodo/qposlib/QPosManager.java
@@ -210,19 +210,12 @@ public class QPosManager<T extends DspreadDevicePOS> extends AbstractDongle impl
             mPosService.setCardTradeMode(qposParameters.getCardTradeMode());
             mPosService.setAmountIcon(transactionAmountData.getAmountIcon());
             mPosService.setAmountPoint(qposParameters.getExponent() > 0);
-            String amount = transactionAmountData.getAmount();
-            String cashback = transactionAmountData.getCashbackAmount();
-            String currencyCode = transactionAmountData.getCurrencyCode();
-            QPOSService.TransactionType transactionType = transactionAmountData.getTransactionType();
 
-            logFlow("onRequestSetAmount(): amount = [" + amount + "], cashback = [" + cashback + "], currencyCode = [" + currencyCode + "], transactionType = [" + transactionType + "]");
-            mPosService.setAmount(setDecimalesAmount(amount), setDecimalesAmount(cashback), currencyCode, transactionType);
-
-           /* if (dongleListener.checkDoTrade()) {
+            if (dongleListener.checkDoTrade()) {
                 mPosService.doTrade(10, 30);
             } else {
                 mPosService.doCheckCard(30, 10);
-            }*/
+            }
         }
     }
 
@@ -694,7 +687,7 @@ public class QPosManager<T extends DspreadDevicePOS> extends AbstractDongle impl
         QPOSService.TransactionType transactionType = transactionAmountData.getTransactionType();
 
         logFlow("onRequestSetAmount(): amount = [" + amount + "], cashback = [" + cashback + "], currencyCode = [" + currencyCode + "], transactionType = [" + transactionType + "]");
-        //mPosService.setAmount(setDecimalesAmount(amount), setDecimalesAmount(cashback), currencyCode, transactionType);
+        mPosService.setAmount(setDecimalesAmount(amount), setDecimalesAmount(cashback), currencyCode, transactionType);
     }
 
     @Override
